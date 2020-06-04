53a3b16792ff9abfab225949bc72725a40bd6c0f
Yang Sun <yangsun@lcodej.com>
2020-06-04 15:59:07 -0700

    create react app, add bootstrap and sass.

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Button from "react-bootstrap/Button"
+
+function App() {
+    return (
+        <div>
+            <p>Hello world!</p>
+            <Button variant="primary">Button</Button>
+        </div>
+    );
+}
+
+export default App
