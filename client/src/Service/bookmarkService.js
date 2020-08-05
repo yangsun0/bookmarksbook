9b9359c924ac9162fd50810f2fa2f1fd4d417375
Yang Sun <yangsun@lcodej.com>
2020-08-05 16:06:38 -0700

    update service

--- client/src/Service/bookmarkService.js
+++ client/src/Service/bookmarkService.js
@@ -1,12 +1,8 @@
 import axios from "axios";
-import type { $AxiosXHR } from "axios";
 import type { AppData } from "../Common/Types";
 
 async function getAppData(): Promise<AppData> {
-  const query: Promise<$AxiosXHR<AppData>> = axios(
-    "http://localhost:3000/data"
-  );
-  const result = await query;
+  const result = await axios.get("http://localhost:3000/data");
   return result.data;
 }
 
